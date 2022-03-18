7af32ba452ca5bd7346da6f3d00337e116f72b5c
luiz.felipe.farias@ccc.ufcg.edu.br <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>
2022-03-18 14:17:56 -0300

    Removendo entidades não utilizada

--- src/main/java/com/psoft/tccmatch/model/Administrador.java
+++
@@ -1,8 +0,0 @@
-package com.psoft.tccmatch.model;
-
-public class Administrador extends CargoAdministrativoAbstract {
-
-    public Administrador(String nome, String email, String senha) {
-        super(nome, email, senha);
-    }
-}
--- src/main/java/com/psoft/tccmatch/model/Coodernador.java
+++
@@ -1,8 +0,0 @@
-package com.psoft.tccmatch.model;
-
-public class Coodernador extends CargoAdministrativoAbstract {
-
-    public Coodernador(String nome, String email, String senha) {
-        super(nome, email, senha);
-    }
-}
