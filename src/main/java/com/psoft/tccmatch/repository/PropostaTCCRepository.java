da5a20fe87cc06d17eff69ac1d1bbdf39bfd1bce
luiz.felipe.farias@ccc.ufcg.edu.br <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>
2022-03-22 18:17:37 -0300

    Find all PropostaTCC that have a professor

--- src/main/java/com/psoft/tccmatch/repository/PropostaTCCRepository.java
+++ src/main/java/com/psoft/tccmatch/repository/PropostaTCCRepository.java
@@ -2,10 +2,15 @@ package com.psoft.tccmatch.repository;
 
 import com.psoft.tccmatch.model.PropostaTCC;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PropostaTCCRepository extends JpaRepository<PropostaTCC, Long> {
     Optional<PropostaTCC> findByTitulo(String titulo);
 
+    @Query("SELECT p FROM PropostaTCC p JOIN p.professor WHERE p.professor IS NOT NULL")
+    List<PropostaTCC> findCriadoByProfessor();
 }
