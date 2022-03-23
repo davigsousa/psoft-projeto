f30fa867154a029d6756e70164d3c5cf76425634
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-03-23 16:41:40 -0300

    feat: add dto de resposta para aluno

--- src/main/java/com/psoft/tccmatch/DTO/AlunoDTO.java
+++ src/main/java/com/psoft/tccmatch/DTO/AlunoDTO.java
@@ -1,5 +1,10 @@
 package com.psoft.tccmatch.DTO;
 
+
+import com.psoft.tccmatch.model.Aluno;
+
+import java.util.List;
+
 public class AlunoDTO {
     private String nome;
     private String email;
@@ -26,5 +31,19 @@ public class AlunoDTO {
 
     public String getSenha() {
         return senha;
+    }
+
+    public static class RespostaApi {
+        public String nome;
+        public String email;
+        public String matricula;
+        public String periodo_de_conclusao;
+
+        public RespostaApi(Aluno aluno) {
+            this.nome = aluno.getNome();
+            this.email = aluno.getEmail();
+            this.matricula = aluno.getMatricula();
+            this.periodo_de_conclusao = aluno.getPeriodoDeConclusao();
+        }
     }
 }
