1f9e0655765661fd7742c3e96a0ffca943ed7138
marianesc <mariane.carvalho@ccc.ufcg.edu.br>
2022-03-23 17:09:57 -0300

    refactor AlunoController

--- src/main/java/com/psoft/tccmatch/controller/AlunoController.java
+++ src/main/java/com/psoft/tccmatch/controller/AlunoController.java
@@ -3,7 +3,7 @@ package com.psoft.tccmatch.controller;
 import com.psoft.tccmatch.DTO.AlunoDTO;
 import com.psoft.tccmatch.exception.ApiException;
 import com.psoft.tccmatch.model.Aluno;
-import com.psoft.tccmatch.service.AlunoServiceImpl;
+import com.psoft.tccmatch.service.AlunoService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +17,20 @@ import java.util.stream.Collectors;
 @RestController
 public class AlunoController {
     @Autowired
-    private AlunoServiceImpl alunoServiceImpl;
+    private AlunoService alunoService;
 
     @RequestMapping(path = "/alunos", method = RequestMethod.POST)
     @PreAuthorize("hasAuthority('ADMIN')")
     @Transactional
     public ResponseEntity<?> criarAluno(@RequestBody AlunoDTO alunoDTO) throws ApiException {
-        Aluno result = alunoServiceImpl.criar(alunoDTO);
+        Aluno result = alunoService.criar(alunoDTO);
         return ResponseEntity.ok(new AlunoDTO.RespostaApi(result));
     }
 
     @RequestMapping(path = "/alunos", method = RequestMethod.GET)
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<?> getAlunos() {
-        List<Aluno> result = alunoServiceImpl.getAll();
+        List<Aluno> result = alunoService.getAll();
 
         List<AlunoDTO.RespostaApi> resultList = result.stream().map(AlunoDTO.RespostaApi::new).collect(Collectors.toList());
         return ResponseEntity.ok(resultList);
@@ -39,21 +39,21 @@ public class AlunoController {
     @RequestMapping(path = "/alunos/{matricula}", method = RequestMethod.GET)
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<?> getAluno(@PathVariable("matricula") String matricula) throws ApiException {
-        Aluno result = alunoServiceImpl.get(matricula);
+        Aluno result = alunoService.get(matricula);
         return ResponseEntity.ok(new AlunoDTO.RespostaApi(result));
     }
 
     @RequestMapping(path = "/alunos", method = RequestMethod.PUT)
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<?> editarAluno(@RequestBody AlunoDTO alunoDTO) throws ApiException {
-        Aluno result = alunoServiceImpl.editar(alunoDTO);
+        Aluno result = alunoService.editar(alunoDTO);
         return ResponseEntity.ok(new AlunoDTO.RespostaApi(result));
     }
 
     @RequestMapping(path = "/alunos/{matricula}", method = RequestMethod.DELETE)
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<?> deletarAluno(@PathVariable("matricula") String matricula) throws ApiException {
-        alunoServiceImpl.remover(matricula);
+        alunoService.remover(matricula);
         return (ResponseEntity<?>) ResponseEntity.status(204);
     }
 }
