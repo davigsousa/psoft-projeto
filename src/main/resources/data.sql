5b78b9140ab80050dc85f1b826c03f10290a907c
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-03-22 22:13:33 -0300

    feat: add jwt request filter

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,5 +7,6 @@ values(2, 'CODEX');
 insert into laboratorio (ID, NOME)
 values(3, 'EMBEDDED');
 
+/* A senha é 123456 */
 insert into Admin_User (ID, EMAIL, SENHA)
-values(1, 'admin@email.com', '123456');
+values(1, 'admin@email.com', '$2a$10$nnYH.BGaHeidn7EufsgKg.MXJ2zQiUKIVdiR89SRNwHg9u0FmsGyy');
