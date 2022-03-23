ca58c37309b9ee53a9fc24c4e552d3af0cb18edb
marianesc <mariane.carvalho@ccc.ufcg.edu.br>
2022-03-23 19:04:57 -0300

    adiciona dados de aluno e area de estudo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,10 @@ values(3, 'EMBEDDED');
 /* A senha é 123456 */
 insert into Admin_User (ID, EMAIL, SENHA)
 values(1, 'admin@email.com', '$2a$10$nnYH.BGaHeidn7EufsgKg.MXJ2zQiUKIVdiR89SRNwHg9u0FmsGyy');
+
+/* A senha é 123456 */
+insert into aluno (ID, NOME, MATRICULA, EMAIL, SENHA, PERIODO_DE_CONCLUSAO)
+values(4, 'Maria', '2345646', 'maria@gmail.com', '$2a$10$nnYH.BGaHeidn7EufsgKg.MXJ2zQiUKIVdiR89SRNwHg9u0FmsGyy', '2025.2');
+
+insert into Area_Estudo (ID, ASSUNTO)
+values(5, 'Projeto de Software');
