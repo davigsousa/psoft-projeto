0de251ff35f1a49b966af5408ed632276901aa11
luiz.felipe.farias@ccc.ufcg.edu.br <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>
2022-03-24 17:00:55 -0300

    novos elementos no data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,3 +16,10 @@ values(1, 'Luiz', 'luiz@mail.com', '119210052', '$2a$10$nnYH.BGaHeidn7EufsgKg.MX
 
 insert into Professor (ID, NOME, EMAIL, SENHA, MAX_ORIENTANDOS)
 values(1, 'Luiz Prof', 'luiz@prof.com', '$2a$10$nnYH.BGaHeidn7EufsgKg.MXJ2zQiUKIVdiR89SRNwHg9u0FmsGyy', 2);
+
+/* A senha é 123456 */
+insert into aluno (ID, NOME, MATRICULA, EMAIL, SENHA, PERIODO_DE_CONCLUSAO)
+values(4, 'Maria', '2345646', 'maria@gmail.com', '$2a$10$nnYH.BGaHeidn7EufsgKg.MXJ2zQiUKIVdiR89SRNwHg9u0FmsGyy', '2025.2');
+
+insert into Area_Estudo (ID, ASSUNTO)
+values(5, 'Projeto de Software');
