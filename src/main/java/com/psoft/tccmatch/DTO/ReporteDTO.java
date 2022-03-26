7121455634f47b93550c8dce2941e4998fd582d4
luiz.felipe.farias@ccc.ufcg.edu.br <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>
2022-03-26 18:25:42 -0300

    new return for report list

--- src/main/java/com/psoft/tccmatch/DTO/ReporteDTO.java
+++ src/main/java/com/psoft/tccmatch/DTO/ReporteDTO.java
@@ -2,6 +2,9 @@ package com.psoft.tccmatch.DTO;
 
 import com.psoft.tccmatch.model.Reporte;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReporteDTO {
     private String periodo;
     private String problema;
@@ -17,14 +20,40 @@ public class ReporteDTO {
     public static class RespostaAPI {
         public String periodo;
         public String problema;
-        public String usuario;
+        public String criador;
         public String orientacao;
 
         public RespostaAPI(Reporte reporte) {
             this.periodo = reporte.getPeriodo();
             this.problema = reporte.getProblema();
-            this.usuario = reporte.getUsuario();
+            this.criador = getCriador(reporte);
             this.orientacao = reporte.getOrientacao().toString();
+        }
+
+        private String getCriador(Reporte reporte) {
+            if (reporte.getAluno() != null) {
+                return reporte.getAluno().toString();
+            } else {
+                return reporte.getProfessor().toString();
+            }
+        }
+    }
+
+    static public class RespostaApiLista {
+        public List<ReporteDTO.RespostaAPI> alunos;
+        public List<ReporteDTO.RespostaAPI> professores;
+
+        public RespostaApiLista(List<Reporte> alunos, List<Reporte> professores) {
+            this.alunos = this.formataLista(alunos);
+            this.professores = this.formataLista(professores);
+        }
+
+        private List<ReporteDTO.RespostaAPI> formataLista(List<Reporte> list) {
+            List<ReporteDTO.RespostaAPI> formatado = new ArrayList<>();
+            for (Reporte reporte : list) {
+                formatado.add(new ReporteDTO.RespostaAPI(reporte));
+            }
+            return formatado;
         }
     }
 }
