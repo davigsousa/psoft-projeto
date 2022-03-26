2bdb286e6f88f60f76eac1fc784fb6b306e987eb
luiz.felipe.farias@ccc.ufcg.edu.br <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>
2022-03-26 18:25:55 -0300

    querying reports

--- src/main/java/com/psoft/tccmatch/repository/ReporteRepository.java
+++ src/main/java/com/psoft/tccmatch/repository/ReporteRepository.java
@@ -1,16 +1,31 @@
 package com.psoft.tccmatch.repository;
 
+import com.psoft.tccmatch.model.Aluno;
 import com.psoft.tccmatch.model.Orientacao;
+import com.psoft.tccmatch.model.Professor;
 import com.psoft.tccmatch.model.Reporte;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReporteRepository extends JpaRepository<Reporte, Long> {
-    Optional<Reporte> findByUsuarioAndPeriodoAndProblemaAndOrientacao(
-            String usuario,
+    Optional<Reporte> findByAlunoAndPeriodoAndProblemaAndOrientacao(
+            Aluno aluno,
             String periodo,
             String problema,
             Orientacao orientacao
     );
+
+    Optional<Reporte> findByProfessorAndPeriodoAndProblemaAndOrientacao(
+            Professor professor,
+            String periodo,
+            String problema,
+            Orientacao orientacao
+    );
+
+    List<Reporte> findAllByPeriodoAndProfessorIsNotNull(String periodo);
+
+    List<Reporte> findAllByPeriodoAndAlunoIsNotNull(String periodo);
+
 }
