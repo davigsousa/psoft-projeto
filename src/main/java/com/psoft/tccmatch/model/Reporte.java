59f0f9a0cf533294a5b6bbe7f342abda6b6dd1e1
luiz.felipe.farias@ccc.ufcg.edu.br <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>
2022-03-26 17:00:11 -0300

    Adding Orientacao in Reporting and enhancing constructor

--- src/main/java/com/psoft/tccmatch/model/Reporte.java
+++ src/main/java/com/psoft/tccmatch/model/Reporte.java
@@ -4,17 +4,24 @@ import javax.persistence.*;
 
 @Entity
 public class Reporte {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
     private String periodo;
     private String problema;
+    private String usuario;
+    @ManyToOne
+    private Orientacao orientacao;
 
-    public Reporte(String periodo, String problema) {
+    public Reporte() {
+    }
+
+    public Reporte(String periodo, String problema, Orientacao orientacao, String usuario) {
         this.periodo = periodo;
         this.problema = problema;
+        this.orientacao = orientacao;
+        this.usuario = usuario;
     }
 
     public Long getId() {
@@ -37,4 +44,19 @@ public class Reporte {
         this.problema = problema;
     }
 
+    public Orientacao getOrientacao() {
+        return orientacao;
+    }
+
+    public void setOrientacao(Orientacao orientacao) {
+        this.orientacao = orientacao;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 }
