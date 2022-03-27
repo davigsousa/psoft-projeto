5abcfdc3b537a5de5773b8a207afd2202d8c1888
Matheus Oliveira <matheus.oliveira@ccc.ufcg.edu.br>
2022-03-27 16:35:49 -0300

    fix: removing imports

--- src/main/java/com/psoft/tccmatch/enviadores/CriacaoPropostaTCCEmail.java
+++ src/main/java/com/psoft/tccmatch/enviadores/CriacaoPropostaTCCEmail.java
@@ -1,13 +1,6 @@
 package com.psoft.tccmatch.enviadores;
 
-import com.psoft.tccmatch.model.Aluno;
-import com.psoft.tccmatch.model.AreaEstudo;
-import com.psoft.tccmatch.service.AlunoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class CriacaoPropostaTCCEmail implements EnviadorEmail {
