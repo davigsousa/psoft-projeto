67e21c8e562ffc403b74561265ec7acc5773f6e0
marianesc <mariane.carvalho@ccc.ufcg.edu.br>
2022-03-27 15:19:52 -0300

    remove optional id

--- src/main/java/com/psoft/tccmatch/DTO/ProfessorDTO.java
+++ src/main/java/com/psoft/tccmatch/DTO/ProfessorDTO.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public class ProfessorDTO {
-    private Optional<Long> id;
     private String nome;
     private String email;
     private List<Long> laboratorios;
@@ -32,10 +31,6 @@ public class ProfessorDTO {
 
     public List<Long> getLaboratorios() {
         return laboratorios;
-    }
-
-    public Long getId() {
-        return this.id.orElse(null);
     }
 
     public static class RespostaApi {
